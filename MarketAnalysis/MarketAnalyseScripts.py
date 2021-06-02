de0684001c136082cb9c623496ed77afe77eacd9
Kumar Madhavan <kumar.madhavan@gmail.com>
2021-06-02 20:20:49 +0530

    Added ADX index to the list for further analysis.

--- MarketAnalysis/MarketAnalyseScripts.py
+++ MarketAnalysis/MarketAnalyseScripts.py
@@ -4,6 +4,7 @@ import yfinance
 from datetime import datetime
 import os 
 from os import path
+import numpy as np
 
 
 #ScriptList contains the list of script requiring analysis 
@@ -28,7 +29,7 @@ else:
     print ("Successfully created the directory %s " % folderExt)
 
 #Dataframe where the summary for the stock is stored
-analysisResult = pd.DataFrame(columns = ['Stock', 'Close', '12EMA', '26EMA', 'MACDHist', 'SignalMACD', 'CloseSlope', '12EMASlope', '26EMASlope', 'MACDHistSlope', 'SignalMACDSlope', 'PositiveIndicator'])
+analysisResult = pd.DataFrame(columns = ['Stock', 'Close', '12EMA', '26EMA', 'MACDHist', 'SignalMACD', 'ADX', 'CloseSlope', '12EMASlope', '26EMASlope', 'MACDHistSlope', 'SignalMACDSlope', 'ADXSlope', 'PositiveIndicator'])
 
 
 #For each stock the data is retrieved, analyzed and result stored in the stocknameYYYYMMDDHHMMSS.csv file. Summary stored in analysisResultYYYYMMDDHHMMSS.csv
@@ -52,17 +53,63 @@ for stock in stocklist:
         df['26EMASlope'] = df['26EMA'].diff()
         df['MACDHistSlope'] = df['MACDHist'].diff()
         df['SignalMACDSlope'] = df['SignalMACD'].diff()
+
+        # TR
+        alpha = 1/13
+
+        adxdf = df.copy()
+
+
+        adxdf['H-L'] = adxdf['High'] - adxdf['Low']
+        adxdf['H-C'] = np.abs(adxdf['High'] - adxdf['Close'].shift(1))
+        adxdf['L-C'] = np.abs(adxdf['Low'] - adxdf['Close'].shift(1))
+        adxdf['TR'] = adxdf[['H-L', 'H-C', 'L-C']].max(axis=1)
+        del adxdf['H-L'], adxdf['H-C'], adxdf['L-C']
+
+        adxdf['ATR'] = adxdf['TR'].ewm(alpha = alpha, adjust=False).mean()
+        adxdf['H-pH'] = adxdf['High'] - adxdf['High'].shift(1)
+        adxdf['pL-L'] = adxdf['Low'].shift(1) - adxdf['Low']
+        adxdf['+DX'] = np.where(
+            (adxdf['H-pH'] > adxdf['pL-L']) & (adxdf['H-pH']>0),
+            adxdf['H-pH'],
+            0.0
+        )
+        adxdf['-DX'] = np.where(
+            (adxdf['H-pH'] < adxdf['pL-L']) & (adxdf['pL-L']>0),
+            adxdf['pL-L'],
+            0.0
+        )
+        del adxdf['H-pH'], adxdf['pL-L']
+
+        # +- DMI
+        adxdf['S+DM'] = adxdf['+DX'].ewm(alpha = alpha, adjust=False).mean()
+        adxdf['S-DM'] = adxdf['-DX'].ewm(alpha = alpha, adjust=False).mean()
+        adxdf['+DMI'] = (adxdf['S+DM']/adxdf['ATR'])*100
+        adxdf['-DMI'] = (adxdf['S-DM']/adxdf['ATR'])*100
+        del adxdf['S+DM'], adxdf['S-DM']
+
+        # ADX
+        adxdf['DX'] = (np.abs(adxdf['+DMI'] - adxdf['-DMI'])/(adxdf['+DMI'] + adxdf['-DMI']))*100
+        adxdf['ADX'] = adxdf['DX'].ewm(alpha = alpha, adjust=False).mean()
+        del adxdf['DX'], adxdf['ATR'], adxdf['TR'], adxdf['-DX'], adxdf['+DX'], adxdf['+DMI'], adxdf['-DMI']
+
+
+        df['ADX'] = adxdf['ADX']
+        df['ADXSlope'] = df['ADX'].diff()
+
+
         df['PositiveIndicator'] = 0
         df['PositiveIndicator'] = df['PositiveIndicator'] + df['CloseSlope'].apply(lambda x: 1 if x > 0 else 0)
         df['PositiveIndicator'] = df['PositiveIndicator'] + df['12EMASlope'].apply(lambda x: 1 if x > 0 else 0)
         df['PositiveIndicator'] = df['PositiveIndicator'] + df['26EMASlope'].apply(lambda x: 1 if x > 0 else 0)
         df['PositiveIndicator'] = df['PositiveIndicator'] + df['MACDHistSlope'].apply(lambda x: 1 if x > 0 else 0)
         df['PositiveIndicator'] = df['PositiveIndicator'] + df['SignalMACDSlope'].apply(lambda x: 1 if x > 0 else 0)
+        df['PositiveIndicator'] = df['PositiveIndicator'] + df['ADXSlope'].apply(lambda x: 1 if x > 0 else 0)
 
         lastRow = df.shape[0]
         currentStockValues = df.iloc[lastRow-1:]
 
-        analysisResult = analysisResult.append( {'Stock' : stock, 'Close' : currentStockValues['Close'][0], '12EMA': currentStockValues['12EMA'][0], '26EMA': currentStockValues['26EMA'][0], 'MACDHist': currentStockValues['MACDHist'][0], 'SignalMACD': currentStockValues['SignalMACD'][0], 'CloseSlope': currentStockValues['CloseSlope'][0], '12EMASlope': currentStockValues['12EMASlope'][0], '26EMASlope': currentStockValues['26EMASlope'][0],  'MACDHistSlope': currentStockValues['MACDHistSlope'][0], 'SignalMACDSlope': currentStockValues['SignalMACDSlope'][0], 'PositiveIndicator' : currentStockValues['PositiveIndicator'][0]}, ignore_index = True)
+        analysisResult = analysisResult.append( {'Stock' : stock, 'Close' : currentStockValues['Close'][0], '12EMA': currentStockValues['12EMA'][0], '26EMA': currentStockValues['26EMA'][0], 'MACDHist': currentStockValues['MACDHist'][0], 'SignalMACD': currentStockValues['SignalMACD'][0], 'CloseSlope': currentStockValues['CloseSlope'][0], 'ADX': currentStockValues['ADX'][0], '12EMASlope': currentStockValues['12EMASlope'][0], '26EMASlope': currentStockValues['26EMASlope'][0],  'MACDHistSlope': currentStockValues['MACDHistSlope'][0], 'SignalMACDSlope': currentStockValues['SignalMACDSlope'][0], 'ADXSlope': currentStockValues['ADXSlope'][0], 'PositiveIndicator' : currentStockValues['PositiveIndicator'][0]}, ignore_index = True)
     except:
         print("Oops!", sys.exc_info()[0], "occurred while processing for stock ", stock, ". Proceeding to next stock")  
 
